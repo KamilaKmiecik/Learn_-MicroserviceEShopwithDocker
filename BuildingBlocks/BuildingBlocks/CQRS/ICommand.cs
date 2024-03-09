5a8e0845cdd6df57cea4020307bd638489ffb199
Kamila <kamilakmiecik01@gmail.com>
2024-03-09 10:41:36 +0100

    Created Building blocks library, added catalog handler

---
+++ BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,13 @@
+﻿global using MediatR; 
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommand: IRequest<Unit>
+{
+
+}
+
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+
+}
